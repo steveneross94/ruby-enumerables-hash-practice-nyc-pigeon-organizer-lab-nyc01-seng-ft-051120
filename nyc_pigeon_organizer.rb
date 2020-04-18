f3a1fe8e2bf993807a660dc0f7567b9169d40f56
Steven Ross <ross.steven@me.com>
2020-04-18 17:31:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+
+        if new_data[name] == nil
+          new_data[name] = {}
+        end
+      end
+    end
+  end
 end
