2baa3ca87a7c73e915466a197caf7ddd4f51cd9f
Steven Ross <ross.steven@me.com>
2020-04-18 17:33:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+
+        if new_data[name] == nil
+          new_data[name] = {}
+        end
+
+        if new_data[name][key] == nil
+          new_data[name][key] = []
+        end
+
+        new_data[name][key] << new_value.to_s
+    end
+  end
+end
+new_data
 end
